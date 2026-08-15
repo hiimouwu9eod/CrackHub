18232e4cf4453e04d44d5844cb993373d1f9c337
hiimouwu9eod <scriptskibs@gmail.com>
2026-08-15 17:15:36 +1000

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,20 @@
+local LoaderFile = {
+    CallBackLoader = {
+        Ids = {
+            75366259315586, -- Build Ur Base
+            142823291       -- Murder Mystery 2
+        }
+    }
+}
 
+local Scripts = {
+    [75366259315586] = "https://raw.githubusercontent.com/hiimouwu9eod/CrackHub/refs/heads/main/bulidurbase.lua",
+    [142823291] = "https://raw.githubusercontent.com/hiimouwu9eod/CrackHub/refs/heads/main/mm2.lua"
+}
+
+local placeId = game.PlaceId
+local url = Scripts[placeId]
+
+if url then
+    loadstring(game:HttpGet(url, true))()
+end
