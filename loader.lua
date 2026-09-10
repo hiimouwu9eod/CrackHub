83de66a2d92fe2ddb92307603b1eff1ab0401592
hiimouwu9eod <scriptskibs@gmail.com>
2026-09-10 10:31:52 +1000

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,14 +1,14 @@
 local Scripts = {
-    [75366259315586] = "https://raw.githubusercontent.com/hiimouwu9eod/CrackHub/refs/heads/main/bulidurbase.lua", -- Build Ur Base
-    [142823291] = "https://raw.githubusercontent.com/hiimouwu9eod/CrackHub/refs/heads/main/mm2.lua" -- Murder Mystery 2
-    [10253248401] = "https://raw.githubusercontent.com/hiimouwu9eod/CrackHub/refs/heads/main/ElementTycoon.lua" -- Element Tycoon
+	[75366259315586] = "https://raw.githubusercontent.com/hiimouwu9eod/CrackHub/refs/heads/main/bulidurbase.lua", -- Build Ur Base
+	[142823291] = "https://raw.githubusercontent.com/hiimouwu9eod/CrackHub/refs/heads/main/mm2.lua", -- Murder Mystery 2
+	[10253248401] = "https://raw.githubusercontent.com/hiimouwu9eod/CrackHub/refs/heads/main/ElementTycoon.lua", -- Element Tycoon
 }
 
 local placeId = game.PlaceId
 local url = Scripts[placeId]
 
 if url then
-    loadstring(game:HttpGet(url, true))()
+	loadstring(game:HttpGet(url, true))()
 else
-    warn("[Loader] No script for PlaceId:", placeId)
+	warn("[Loader] No script for PlaceId:", placeId)
 end
